3d15b7166bf7827e5f0207b556945ae921739bc3
t-roholt <rjmholt@gmail.com>
2017-01-14 15:51:22 -0800

    Changed DSL Keyword design. Added convenience method for parameter parsing.

--- src/vs-csproj/engine/parser/DslSpecification.cs
+++ src/vs-csproj/engine/parser/DslSpecification.cs
@@ -2,56 +2,177 @@
 Copyright (c) Microsoft Corporation.  All rights reserved.
 --********************************************************************/
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+// TODO:
+//   - Check ParseError errorId strings
+//   - Check ParseError Extents
+
 namespace System.Management.Automation.Language
 {
     /// <summary>
-    /// An interface specifying the semantic properties/actions that a
-    /// DSL keyword must provide to the PowerShell runtime. All keyword
-    /// additions to the language must implement this.
+    /// Specifies the semantic properties/actions that a
+    /// DSL keyword must provide to the PowerShell runtime. This must
+    /// be subclassed to instantiate a new PowerShell keyword
     /// </summary>
-    interface IPSDslKeyword
+    public abstract class Keyword
     {
-        /// <summary>
-        /// The pre-parse action of the keyword. Implemented
-        /// as a Func property to allow nullability.
-        /// </summary>
-        Func<DynamicKeyword, ParseError[]> PreParse
+        protected Keyword() { }
+
+        public virtual Func<DynamicKeyword, ParseError[]> PreParse
+        {
+            get;
+        }
+
+        public virtual Func<DynamicKeywordStatementAst, ParseError[]> PostParse
+        {
+            get;
+        }
+
+        public virtual Func<DynamicKeywordStatementAst, ParseError[]> SemanticCheck
         {
             get;
         }
 
         /// <summary>
-        /// The post-parse action of the keyword. Implemented
-        /// as a Func property to allow nullability.
+        /// A default, convenience implementation for parameter resolution. Provided
+        /// so that keyword implementors do not need to always reimplement most likely
+        /// functionality.
         /// </summary>
-        Func<DynamicKeywordStatementAst, ParseError[]> PostParse
+        /// <param name="kwStmtAst"></param>
+        /// <returns></returns>
+        protected virtual ParseError[] ResolveParameters(DynamicKeywordStatementAst kwAst)
         {
-            get;
+            var errorList = new List<ParseError>();
+
+            Type keywordType = this.GetType();
+
+            var commandElements = Ast.CopyElements(kwAst.CommandElements);
+            var commandAst = new CommandAst(kwAst.Extent, commandElements, TokenKind.Unknown, null);
+            StaticBindingResult bindingResult = StaticParameterBinder.BindCommand(commandAst);
+
+            // Fetch all keyword parameter properties
+            IEnumerable<PropertyInfo> kwParams = from p in keywordType.GetProperties()
+                                                 where p.IsDefined(typeof(KeywordParameterAttribute))
+                                                 select p;
+
+            // First check we have all the parameters we need
+            var mandatoryParamQuery = from p in kwParams
+                                      where p.GetCustomAttribute<KeywordParameterAttribute>().Mandatory
+                                      select new { parameter = p, position = p.GetCustomAttribute<KeywordParameterAttribute>().Postion };
+
+            bool missingParams = false;
+            foreach (var paramData in mandatoryParamQuery)
+            {
+                // Check if the parameter value is specified by name
+                if (!bindingResult.BoundParameters.ContainsKey(paramData.parameter.Name))
+                {
+                    // Otherwise check it was provided positionally
+                    if (!bindingResult.BoundParameters.ContainsKey(paramData.position.ToString()))
+                    {
+                        missingParams = true;
+                        errorList.Add(new ParseError(kwAst.Extent, "MissingParameter", "The parameter " + paramData.parameter.Name + " was not provided"));
+                    }
+                }
+            }
+
+            if (missingParams)
+            {
+                return errorList.ToArray();
+            }
+
+            // Now take all the arguments that were bound and fill the instance with them, checking
+            // for surplus parameters as we go
+            bool badParams = false;
+            foreach (KeyValuePair<string, ParameterBindingResult> binding in bindingResult.BoundParameters)
+            {
+                PropertyInfo propInfo;
+
+                // Deal with the possibility of positional parameters
+                int position;
+                if (Int32.TryParse(binding.Key, out position))
+                {
+                    propInfo = kwParams.FirstOrDefault(p => p.GetCustomAttribute<KeywordParameterAttribute>().Postion == position);
+                    if (propInfo == null)
+                    {
+                        badParams = true;
+                        errorList.Add(new ParseError(binding.Value.Value.Extent, "UnknownParameter", "The parameter at position " + binding.Key + " was not recognized"));
+                    }
+                    else
+                    {
+                        badParams = !TrySetValue(propInfo, binding.Value, errorList);
+                    }
+                }
+                // Deal with named parameters
+                else
+                {
+                    propInfo = keywordType.GetProperty(binding.Key, BindingFlags.IgnoreCase);
+                    if (propInfo == null)
+                    {
+                        badParams = true;
+                        errorList.Add(new ParseError(binding.Value.Value.Extent, "UnknownParameter", "The parameter at position " + binding.Key + " was not recognized"));
+                    }
+                    else
+                    {
+                        badParams = !TrySetValue(propInfo, binding.Value, errorList);
+                    }
+                }
+            }
+
+            if (badParams)
+            {
+                return errorList.ToArray();
+            }
+
+            // Signal no errors by returning null
+            return null;
         }
 
         /// <summary>
-        /// The semantic checking operation to make at parse time for
-        /// a DSL keyword. Implemented as a Func property so as to be
-        /// nullable.
+        /// Tries to set the property specified by property value to the
+        /// parameter-bound value in boundValue
         /// </summary>
-        Func<DynamicKeywordStatementAst, ParseError[]> SemanticCheck
+        /// <param name="propInfo">metadata about the property being set</param>
+        /// <param name="boundValue">powershell bound parameter containing the value to set</param>
+        /// <param name="errorList">list of errors so far, to add to</param>
+        /// <returns></returns>
+        protected bool TrySetValue(PropertyInfo propInfo, ParameterBindingResult boundValue, List<ParseError> errorList)
         {
-            get;
+            // Ensure the property is writeable
+            if (!propInfo.CanWrite)
+            {
+                errorList.Add(new ParseError(boundValue.Value.Extent, "UnwritableProperty", "The property " + propInfo.Name + " is not able to be written"));
+                return false;
+            }
+
+            // Try to resolve a value from the argument's Ast node
+            object astValue;
+            try
+            {
+                astValue = boundValue.Value.SafeGetValue();
+            }
+            catch (InvalidOperationException e)
+            {
+                errorList.Add(new ParseError(boundValue.Value.Extent, "UnsafeValue", "The value " + boundValue.Value.ToString() + " could not be parsed"));
+                return false;
+            }
+
+            // Try to coerce the value's type to that of the property
+            var value = Convert.ChangeType(astValue, propInfo.PropertyType);
+            if (value == null)
+            {
+                errorList.Add(new ParseError(boundValue.Value.Extent, "BadParameterType", "The parameter " + boundValue.Value.ToString() + " was of incorrect type"));
+                return false;
+            }
+
+            propInfo.SetValue(this, value);
+            return true;
         }
     }
 
     #region DSL definition attributes
-
-    /// <summary>
-    /// Specifies a naming requirement for a keyword when used.
-    /// The default is no name.
-    /// </summary>
-    public enum KeywordNameMode
-    {
-        NoName,   // No name should be given when calling this keyword
-        Required, // A name must be given
-        Optional, // A name may be given
-    }
 
     /// <summary>
     /// Specifies the syntax of the body following a keyword.
@@ -66,43 +187,29 @@ namespace System.Management.Automation.Language
 
     /// <summary>
     /// Specifies the number of times a keyword may be used in a block.
-    /// Defaults to Required.
+    /// Defaults to Optional.
     /// </summary>
     public enum KeywordUseMode
     {
-        Required,     // Must be used exactly once
-        RequiredMany, // Must be used at least once
         Optional,     // May be used 0-1 times
         OptionalMany, // May be used any number of times
+        Required,     // Must be used exactly once
+        RequiredMany, // Must be used at least once
     }
-
-    /// <summary>
-    /// Defines a class as a definition block for a PowerShell DSL
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
-    public class PSDslAttribute : System.Attribute {}
 
     /// <summary>
     /// Specifies that a class denotes a DSL Keyword
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
-    public class PSKeywordAttribute : System.Attribute
+    public class KeywordAttribute : System.Attribute
     {
-        private KeywordNameMode nameMode;
         private KeywordBodyMode bodyMode;
         private KeywordUseMode useMode;
 
-        public PSKeywordAttribute()
+        public KeywordAttribute()
         {
-            this.nameMode = KeywordNameMode.NoName;
             this.bodyMode = KeywordBodyMode.Command;
-            this.useMode = KeywordUseMode.Required;
-        }
-
-        public virtual KeywordNameMode Name
-        {
-            get { return nameMode; }
-            set { nameMode = value; }
+            this.useMode = KeywordUseMode.Optional;
         }
 
         public virtual KeywordBodyMode Body
@@ -121,26 +228,16 @@ namespace System.Management.Automation.Language
     /// <summary>
     /// Specifies a field denoting a keyword argument
     /// </summary>
-    [AttributeUsage(AttributeTargets.Field)]
-    public class PSKeywordArgumentAttribute : System.Attribute
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class KeywordParameterAttribute : System.Attribute
     {
-        private string name;
         private bool mandatory;
+        private int position;
 
-        public PSKeywordArgumentAttribute()
+        public KeywordParameterAttribute()
         {
-            this.name = null;
             this.mandatory = true;
-        }
-
-        /// <summary>
-        /// If this is not null or empty, sets the parameter name for the argument,
-        /// allowing it to be passed as a named argument.
-        /// </summary>
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
+            this.position = -1;
         }
 
         /// <summary>
@@ -151,6 +248,16 @@ namespace System.Management.Automation.Language
         {
             get { return mandatory; }
             set { mandatory = value; }
+        }
+
+        /// <summary>
+        /// Specifies what position a parameter occurs at if not passed by name.
+        /// A value of -1 means this parameter may be passed by name only.
+        /// </summary>
+        public int Postion
+        {
+            get { return position; }
+            set { position = value; }
         }
     }
 
