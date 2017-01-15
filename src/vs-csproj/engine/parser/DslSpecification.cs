274691fb0b519dbe0e9d18e9bb5042bc74e04b03
t-roholt <rjmholt@gmail.com>
2017-01-14 17:23:16 -0800

    Improved default parameter parsing implementation

--- src/vs-csproj/engine/parser/DslSpecification.cs
+++ src/vs-csproj/engine/parser/DslSpecification.cs
@@ -46,93 +46,135 @@ namespace System.Management.Automation.Language
         /// so that keyword implementors do not need to always reimplement most likely
         /// functionality.
         /// </summary>
-        /// <param name="kwStmtAst"></param>
+        /// <param name="kwStmtAst">dynamic keyword statement ast node</param>
         /// <returns></returns>
         protected virtual ParseError[] ResolveParameters(DynamicKeywordStatementAst kwAst)
         {
-            var errorList = new List<ParseError>();
+            IEnumerable<PropertyInfo> allKwParams = from p in this.GetType().GetProperties()
+                                                    where p.IsDefined(typeof(KeywordParameterAttribute))
+                                                    select p;
 
-            Type keywordType = this.GetType();
+            return ResolveParameters(kwAst, allKwParams);
+        }
+
+        /// <summary>
+        /// A parameter resolving method to set all properties in a given list, assuming they use
+        /// default property getter/setter implementations.
+        /// </summary>
+        /// <param name="kwAst">dynamic keyword statement ast node</param>
+        /// <param name="autoSetKeywords">the keywords to be set using this unclever method</param>
+        /// <returns>a list of errors encountered while parsing, or null on success</returns>
+        protected virtual ParseError[] ResolveParameters(DynamicKeywordStatementAst kwAst, IEnumerable<PropertyInfo> autoSetKeywords)
+        {
+            var errorList = new List<ParseError>();
 
             var commandElements = Ast.CopyElements(kwAst.CommandElements);
             var commandAst = new CommandAst(kwAst.Extent, commandElements, TokenKind.Unknown, null);
             StaticBindingResult bindingResult = StaticParameterBinder.BindCommand(commandAst);
 
-            // Fetch all keyword parameter properties
-            IEnumerable<PropertyInfo> kwParams = from p in keywordType.GetProperties()
-                                                 where p.IsDefined(typeof(KeywordParameterAttribute))
-                                                 select p;
-
-            // First check we have all the parameters we need
-            var mandatoryParamQuery = from p in kwParams
-                                      where p.GetCustomAttribute<KeywordParameterAttribute>().Mandatory
-                                      select new { parameter = p, position = p.GetCustomAttribute<KeywordParameterAttribute>().Postion };
-
-            bool missingParams = false;
-            foreach (var paramData in mandatoryParamQuery)
-            {
-                // Check if the parameter value is specified by name
-                if (!bindingResult.BoundParameters.ContainsKey(paramData.parameter.Name))
-                {
-                    // Otherwise check it was provided positionally
-                    if (!bindingResult.BoundParameters.ContainsKey(paramData.position.ToString()))
-                    {
-                        missingParams = true;
-                        errorList.Add(new ParseError(kwAst.Extent, "MissingParameter", "The parameter " + paramData.parameter.Name + " was not provided"));
-                    }
-                }
-            }
-
-            if (missingParams)
+            // Ensure we have values for the all parameters we are responsible for setting
+            if (IsMissingMandatoryParams(autoSetKeywords, bindingResult.BoundParameters, kwAst.Extent, errorList))
             {
                 return errorList.ToArray();
             }
 
-            // Now take all the arguments that were bound and fill the instance with them, checking
-            // for surplus parameters as we go
-            bool badParams = false;
+            // Try and set all the parameters that were passed in
             foreach (KeyValuePair<string, ParameterBindingResult> binding in bindingResult.BoundParameters)
             {
-                PropertyInfo propInfo;
-
-                // Deal with the possibility of positional parameters
-                int position;
-                if (Int32.TryParse(binding.Key, out position))
+                if (!TrySetNamedParam(binding, errorList) && !TrySetPositionalParam(autoSetKeywords, binding, errorList))
                 {
-                    propInfo = kwParams.FirstOrDefault(p => p.GetCustomAttribute<KeywordParameterAttribute>().Postion == position);
-                    if (propInfo == null)
-                    {
-                        badParams = true;
-                        errorList.Add(new ParseError(binding.Value.Value.Extent, "UnknownParameter", "The parameter at position " + binding.Key + " was not recognized"));
-                    }
-                    else
-                    {
-                        badParams = !TrySetValue(propInfo, binding.Value, errorList);
-                    }
-                }
-                // Deal with named parameters
-                else
-                {
-                    propInfo = keywordType.GetProperty(binding.Key, BindingFlags.IgnoreCase);
-                    if (propInfo == null)
-                    {
-                        badParams = true;
-                        errorList.Add(new ParseError(binding.Value.Value.Extent, "UnknownParameter", "The parameter at position " + binding.Key + " was not recognized"));
-                    }
-                    else
-                    {
-                        badParams = !TrySetValue(propInfo, binding.Value, errorList);
-                    }
+                    errorList.Add(new ParseError(binding.Value.Value.Extent, "UnknownParameter", "The parameter at position " + binding.Key + " was not recognized"));
                 }
             }
 
-            if (badParams)
+            return errorList.ToArray();
+        }
+
+        /// <summary>
+        /// Checks that all mandatory parameters have been provided
+        /// </summary>
+        /// <param name="propertiesToSet">the properties the the caller intends to set</param>
+        /// <param name="boundParameters">the parameters bound in PowerShell</param>
+        /// <param name="extent">the extent of the keyword statement</param>
+        /// <param name="errorList">the list of errors so far</param>
+        /// <returns></returns>
+        protected bool IsMissingMandatoryParams(IEnumerable<PropertyInfo> propertiesToSet, Dictionary<string, ParameterBindingResult> boundParameters, IScriptExtent extent, List<ParseError> errorList)
+        {
+            bool hasMissing = false;
+            foreach (var propInfo in propertiesToSet)
             {
-                return errorList.ToArray();
+                var paramOptions = propInfo.GetCustomAttribute<KeywordParameterAttribute>();
+                if (paramOptions.Mandatory)
+                {
+                    if (!boundParameters.ContainsKey(propInfo.Name) && !boundParameters.ContainsKey(paramOptions.Postion.ToString()))
+                    {
+                        hasMissing = true;
+                        errorList.Add(new ParseError(extent, "MissingParameter", "The parameter " + propInfo.Name + " was not provided"));
+                    }
+                }
             }
 
-            // Signal no errors by returning null
-            return null;
+            return hasMissing;
+        }
+
+        /// <summary>
+        /// Attempt to set a bound parameter on the assumption it is bound to a position
+        /// </summary>
+        /// <param name="possibleParams">the parameters the positional parameter could correspond to</param>
+        /// <param name="boundParameter">the positional parameter binding</param>
+        /// <param name="errorList">the list of errors so far</param>
+        /// <returns>true if the value was set correctly, false otherwise</returns>
+        protected bool TrySetPositionalParam(IEnumerable<PropertyInfo> possibleParams, KeyValuePair<string, ParameterBindingResult> boundParameter, List<ParseError> errorList)
+        {
+            // Reject null parameters
+            if (possibleParams == null)
+            {
+                throw new ArgumentNullException("possibleParams");
+            }
+
+            if (errorList == null)
+            {
+                throw new ArgumentNullException("errorList");
+            }
+
+            // Attempt to parse the key as an int
+            int position;
+            if (!Int32.TryParse(boundParameter.Key, out position))
+            {
+                return false;
+            }
+
+            // Find the property corresponding to the position if one exists, and set it
+            PropertyInfo propInfo = possibleParams.FirstOrDefault(p => p.GetCustomAttribute<KeywordParameterAttribute>().Postion == position);
+            if (propInfo == null)
+            {
+                return false;
+            }
+
+            return TrySetValue(propInfo, boundParameter.Value, errorList);
+        }
+
+        /// <summary>
+        /// Try to set a named parameter, assuming that the parameter key must be its name
+        /// </summary>
+        /// <param name="boundParameter">the bound parameter</param>
+        /// <param name="errorList">the list of errors so far</param>
+        /// <returns>true if the value was set correctly, false otherwise</returns>
+        protected bool TrySetNamedParam(KeyValuePair<string, ParameterBindingResult> boundParameter, List<ParseError> errorList)
+        {
+            // Reject null parameters
+            if (errorList == null)
+            {
+                throw new ArgumentNullException("errorList");
+            }
+
+            PropertyInfo propInfo = this.GetType().GetProperty(boundParameter.Key, BindingFlags.IgnoreCase);
+            if (propInfo == null)
+            {
+                return false;
+            }
+
+            return TrySetValue(propInfo, boundParameter.Value, errorList);
         }
 
         /// <summary>
@@ -142,9 +184,25 @@ namespace System.Management.Automation.Language
         /// <param name="propInfo">metadata about the property being set</param>
         /// <param name="boundValue">powershell bound parameter containing the value to set</param>
         /// <param name="errorList">list of errors so far, to add to</param>
-        /// <returns></returns>
+        /// <returns>true if the value was set correctly, false otherwise</returns>
         protected bool TrySetValue(PropertyInfo propInfo, ParameterBindingResult boundValue, List<ParseError> errorList)
         {
+            // Reject null parameters
+            if (propInfo == null)
+            {
+                throw new ArgumentNullException("propInfo");
+            }
+
+            if (boundValue == null)
+            {
+                throw new ArgumentNullException("boundValue");
+            }
+
+            if (errorList == null)
+            {
+                throw new ArgumentNullException("errorList");
+            }
+
             // Ensure the property is writeable
             if (!propInfo.CanWrite)
             {
